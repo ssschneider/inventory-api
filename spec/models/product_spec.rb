1cd9c792a7ebf965e61727f5fc759f49176c096e
Sarah Schneider <mureb_rosa@hotmail.com>
2025-01-27 16:36:17 -0300

    test: create model test

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -1,5 +1,13 @@
 require 'rails_helper'
 
 RSpec.describe Product, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it "is valid with valid attributes" do
+    product = Product.new(name: "Item 01", quantity: 10, price: 5.99)
+    expect(product).to be_valid
+  end
+
+  it "is not valid without a name" do
+    product = Product.new(name: nil, quantity: 10, price: 5.99)
+    expect(product).to_not be_valid
+  end
 end
